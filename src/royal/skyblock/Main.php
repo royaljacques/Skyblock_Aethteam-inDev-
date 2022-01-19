225507f3f00bf6878287619045a0bd624a68d2be
royal <jacques1.koenig@epitech.eu>
2022-01-19 22:55:07 +0100

    add base

--- src/royal/skyblock/Main.php
+++ src/royal/skyblock/Main.php
@@ -1,1 +1,15 @@
 <?php
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+class Main extends PluginBase implements Listener {
+    public static Main $instance;
+    protected function onEnable (): void
+    {
+        self::$instance = $this;
+    }
+    public static function getInstance(): Main
+    {
+        return self::$instance;
+    }
+}
