0cf2d4506487d0ea1f3d6be978146123aa38af25
BuildTools <jacques1.koenig@epitech.eu>
2022-02-11 00:06:37 +0100

    add just delhome commands (not code in back)

--- src/royal/skyblock/commands/subCommands/DelHome.php
+++ src/royal/skyblock/commands/subCommands/DelHome.php
@@ -1,16 +1,13 @@
 <?php
 namespace royal\skyblock\commands\subCommands;
 
-use CortexPE\Commando\args\BlockPositionArgument;
+
 use CortexPE\Commando\args\RawStringArgument;
 use CortexPE\Commando\BaseSubCommand;
 use CortexPE\Commando\exception\ArgumentOrderException;
-use JsonException;
 use pocketmine\command\CommandSender;
-use pocketmine\player\Player;
-use royal\skyblock\Main;
 
-class SetHome extends BaseSubCommand{
+class DelHome extends BaseSubCommand{
 
     /**
      * @throws ArgumentOrderException
@@ -21,13 +18,8 @@ class SetHome extends BaseSubCommand{
 
     }
 
-    /**
-     * @throws JsonException
-     */
     public function onRun(CommandSender $sender, string $aliasUsed, array $args): void
     {
-        if ($sender instanceof Player){
-            Main::$instance->configAPI->setHome($sender, $args['home_name']);
-        }
+        // TODO: Implement onRun() method.
     }
 }
