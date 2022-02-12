5d6d39372511dd69070bfeb09d5a639b8ca40d9f
kaxyum <98211455+kaxyum@users.noreply.github.com>
2022-02-12 14:56:19 +0100

    Create Help.php

---
+++ src/royal/skyblock/commands/subCommands/Help.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace royal\skyblock\commands\subCommands;
+
+use CortexPE\Commando\args\RawStringArgument;
+use CortexPE\Commando\BaseSubCommand;
+use CortexPE\Commando\exception\ArgumentOrderException;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use royal\skyblock\Main;
+
+class Help extends BaseSubCommand{
+
+    public function onRun(CommandSender $sender, string $aliasUsed, array $args): void
+    {
+        if ($sender instanceof Player){
+            $sender->sendMessage(Main::getInstance()->getLangageAPI()->getTranslate($sender, "help_reply_message"));
+        }
+    }
+}
